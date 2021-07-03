f55d5cdf0f6df6866bbf072e764567b2dfcac0ef
Edi Cziszter <eduard.cziszter@student.upt.ro>
2021-07-03 19:58:45 +0300

    added pages, modules and project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { ArticlesPageComponent } from './pages/articles-page/articles-page.component';
+import { PlayersPageComponent } from './pages/players-page/players-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ContactPageComponent,
+    ArticlesPageComponent,
+    PlayersPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/articles-page/articles-page.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {ArticlesPageComponent} from './articles-page.component';
+
+
+
+@NgModule({
+  declarations: [ArticlesPageComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ArticlesPageModule { }
