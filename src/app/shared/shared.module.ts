6e4613d6acc09106f700071e518cdcd73814761a
Edi Cziszter <eduard.cziszter@student.upt.ro>
2021-07-26 21:45:03 +0300

    footer done + button component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,14 +4,16 @@ import {MenuComponent} from './components/menu/menu.component';
 import {RouterModule} from '@angular/router';
 import {MatIconModule} from "@angular/material/icon";
 import {FooterComponent} from './components/footer/footer.component';
+import { ButtonComponent } from './components/button/button.component';
 
 
 @NgModule({
-  declarations: [MenuComponent, FooterComponent],
-  exports: [
-    MenuComponent,
-    FooterComponent
-  ],
+  declarations: [MenuComponent, FooterComponent, ButtonComponent],
+    exports: [
+        MenuComponent,
+        FooterComponent,
+        ButtonComponent
+    ],
   imports: [
     CommonModule,
     RouterModule,
